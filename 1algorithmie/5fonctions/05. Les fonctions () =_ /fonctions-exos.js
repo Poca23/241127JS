b79d9240ddf56c89976658bf49c4dbce8833ac16
Claire Naudin <cndweb37@gmail.com>
2024-11-27 20:58:04 +0100

    solvedExosFonctionsJs

--- 1algorithmie/5fonctions/05. Les fonctions () =_ /fonctions-exos.js
+++ 1algorithmie/5fonctions/05. Les fonctions () =_ /fonctions-exos.js
@@ -1,21 +1,27 @@
+/**
+ * -------------------------------------------------------
+ * 1 - Écrire une fonction qui prend 2 paramètres et qui retourne la somme de ces 2 paramètres. Afficher le résultat dans la console.
+ * -------------------------------------------------------
+ */
+
+function somme(a, b) {
+  return a + b;
+}
+console.log(somme(1, 2));
 
 /**
-   * -------------------------------------------------------
-   * 1 - Écrire une fonction qui prend 2 paramètres et qui retourne la somme de ces 2 paramètres. Afficher le résultat dans la console.
-   * -------------------------------------------------------
-*/
+ * -------------------------------------------------------
+ * 2 - Écrire une fonction qui prend 2 paramètres et qui retourne la somme de :
+ *  → la raçine carrée du premier paramètres
+ *  → le second paramètre à la puissance 7
+ *  Afficher le résultat dans la console.
+ * -------------------------------------------------------
+ */
 
-
-/**
-   * -------------------------------------------------------
-   * 2 - Écrire une fonction qui prend 2 paramètres et qui retourne la somme de :
-   *  → la raçine carrée du premier paramètres
-   *  → le second paramètre à la puissance 7
-   *  Afficher le résultat dans la console.
-   * -------------------------------------------------------
-*/
-
-
+function somme2(a, b) {
+  return a + b;
+}
+console.log(somme2(Math.sqrt(4), Math.pow(2, 7)));
 /**
    * -------------------------------------------------------
    * 3 - Créer une fonction qui vérifie si l’utilisateur est apte à apprendre du Javascript : 
@@ -25,3 +31,25 @@
     → Si l’exécution de la fonction retourne faux, alors la console affiche “Solidifie tes acquis : rien n’est impossible à qui rêve, ose, travaille et n’abandonne jamais”
    * -------------------------------------------------------
 */
+
+function learnJavascript(langage1, langage2) {
+  if (langage1 === "HTML" && langage2 === "CSS") {
+    return true;
+  } else {
+    return false;
+  }
+}
+let langageHTML = prompt(
+  "Quel est le premier langage informatique à connaître avant d'aborder le JavaScript ?"
+);
+let langageCSS = prompt(
+  "Quel est le DEUXIÈME langage informatique à connaître avant d'aborder le JavaScript ?"
+);
+
+if (learnJavascript(langageHTML, langageCSS)) {
+  console.log("Bienvenue en JS jeune éphèbe");
+} else {
+  console.log(
+    "Solidifie tes acquis : Ose, travaille et n’abandonne jamais. Sois le pélerin qui garde les yeux sur le chemin, et non le sommet de la montagne. Rien n’est impossible à qui rêve"
+  );
+}
