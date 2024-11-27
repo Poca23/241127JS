66f9cdb4de52f3bcd74aa78b3804160caee0cda5
Claire Naudin <cndweb37@gmail.com>
2024-11-27 19:48:01 +0100

    createFolderfonctions-exos

---
+++ 1algorithmie/5fonctions/05. Les fonctions () =_ /fonctions-exos.js
@@ -0,0 +1,27 @@
+
+/**
+   * -------------------------------------------------------
+   * 1 - Écrire une fonction qui prend 2 paramètres et qui retourne la somme de ces 2 paramètres. Afficher le résultat dans la console.
+   * -------------------------------------------------------
+*/
+
+
+/**
+   * -------------------------------------------------------
+   * 2 - Écrire une fonction qui prend 2 paramètres et qui retourne la somme de :
+   *  → la raçine carrée du premier paramètres
+   *  → le second paramètre à la puissance 7
+   *  Afficher le résultat dans la console.
+   * -------------------------------------------------------
+*/
+
+
+/**
+   * -------------------------------------------------------
+   * 3 - Créer une fonction qui vérifie si l’utilisateur est apte à apprendre du Javascript : 
+    avec prompt, demander successivement à l’utilisateur les deux langages de programmation à apprendre avant de démarrer le JS
+    → S’il répond successivement HTML puis CSS, alors la fonction retourne vrai ; sinon faux.
+    → Si l’exécution de la fonction retourne vrai, alors la console affiche “Bienvenue en JS jeune éphèbe”
+    → Si l’exécution de la fonction retourne faux, alors la console affiche “Solidifie tes acquis : rien n’est impossible à qui rêve, ose, travaille et n’abandonne jamais”
+   * -------------------------------------------------------
+*/
